656b84420d0b9b4e105164789a81b1d31bdec3e3
Agustin Cassani <agustin.cassani@leverege.com>
2024-11-22 16:17:03 -0300

    Charts and table data added.

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -13,6 +13,10 @@ import {
   SidebarProvider,
   SidebarTrigger,
 } from "@/components/ui/sidebar"
+import { UltraPieChart } from "@/components/ultra-pie-chart"
+import { UltraRadialChart } from "@/components/ultra-radial-chart"
+import { UltraLineChart } from "@/components/ultra-line-chart"
+import { PaymentsDataTable } from "@/components/payments-table"
 
 export default function Page() {
   return (
@@ -40,11 +44,11 @@ export default function Page() {
         </header>
         <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
           <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-            <div className="aspect-video rounded-xl bg-muted/50" />
-            <div className="aspect-video rounded-xl bg-muted/50" />
-            <div className="aspect-video rounded-xl bg-muted/50" />
+            <UltraPieChart />
+            <UltraRadialChart />
+            <UltraLineChart />
           </div>
-          <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+          <PaymentsDataTable />
         </div>
       </SidebarInset>
     </SidebarProvider>
